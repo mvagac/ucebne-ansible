a5874f739fbaaac4ab68c8d2e0786ec5b1815113
Michal Vagač <michal.vagac@gmail.com>
2024-02-27 09:28:35 +0100

    F137 lookup

--- lookup.sh
+++ lookup.sh
@@ -1,7 +1,8 @@
 
 # hladaj pocitace na F135
 
-IPCKY=$(netdiscover -r 194.160.41.0/24 -P | grep "Micro-Star" | awk '{ print $1 }')
+#IPCKY=$(netdiscover -r 194.160.41.0/24 -P | grep "Micro-Star" | awk '{ print $1 }')
+IPCKY=$(netdiscover -r 192.168.36.0/24 -P | grep "ASRock Incorporation" | awk '{ print $1 }')
 
 
 {
