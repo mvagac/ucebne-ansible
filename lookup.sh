537c34dd6b1cd90027e5892eaadcd24652681617
Michal Vagač <michal.vagac@gmail.com>
2024-02-27 09:49:37 +0100

    lookup pocitacov na danej ucebni; podpora viac ucebni

--- lookup.sh
+++ lookup.sh
@@ -1,12 +1,26 @@
 
-# hladaj pocitace na F135
+# hladaj pocitace na danej ucebni
 
-#IPCKY=$(netdiscover -r 194.160.41.0/24 -P | grep "Micro-Star" | awk '{ print $1 }')
-IPCKY=$(netdiscover -r 192.168.36.0/24 -P | grep "ASRock Incorporation" | awk '{ print $1 }')
+if [ $# -ne 1 ]
+then
+	echo "zadaj ucebnu: f135|f137"
+	exit
+fi
 
+UCEBNA=$1
+
+if [ $UCEBNA == "f135" ]
+then
+	IPCKY=$(netdiscover -r 194.160.41.0/24 -P | grep "Micro-Star" | awk '{ print $1 }')
+fi
+
+if [ $UCEBNA == "f137" ]
+then
+	IPCKY=$(netdiscover -r 192.168.36.0/24 -P | grep "ASRock Incorporation" | awk '{ print $1 }')
+fi
 
 {
-echo "f135:"
+echo "$UCEBNA:"
 echo "  hosts:"
 COUNT=1
 for IP in $IPCKY
@@ -15,5 +29,5 @@ do
 	echo "      ansible_host: ${IP}"
 	COUNT=$((COUNT+1))
 done
-} > inventory-f135.yaml
+} > inventory-${UCEBNA}.yaml
 
